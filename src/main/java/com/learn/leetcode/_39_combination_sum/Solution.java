a33f72bb7763bcfdcb82d1f12e54e628aa5e4b7c
seaXll <2252593269@qq.com>
2020-09-09 22:04:39 +0800

    LeetCode 第 39 题：组合总和 添加 如果不允许重复时候的代码

--- src/main/java/com/learn/leetcode/_39_combination_sum/Solution.java
+++ src/main/java/com/learn/leetcode/_39_combination_sum/Solution.java
@@ -14,38 +14,41 @@ import java.util.List;
  */
 public class Solution {
     private List<List<Integer>> ans = new ArrayList<>();
-
+    private List<Integer> combine = new ArrayList<>();
     private int[] nums;
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<Integer> combine = new ArrayList<>();
         nums = candidates;
-        dfs(target, combine, 0);
+        dfs(target, 0);
         return ans;
     }
 
     /**
      * @param target 剩余需要组合的值
-     * @param combine 当前已经组合的数的列表
      * @param idx   组合 num 的索引
      */
-    public void dfs(int target, List<Integer> combine, int idx) {
-        if (idx == nums.length) {
-            return;
-        }
-
+    public void dfs(int target, int idx) {
         if (target == 0) {
             ans.add(new ArrayList<>(combine));
             return;
         }
 
+        if (idx == nums.length) {
+            return;
+        }
+
+
         // 不选择当前数
-        dfs(target, combine, idx + 1);
+        dfs(target, idx + 1);
         // 选择当前数
         if (target - nums[idx] >= 0) {
             // 使用原引用，提高效率，递归调用前 加入列表，递归调用后，移除加入的一项，避免创建新的 List 浪费空间
             combine.add(nums[idx]);
-            dfs(target - nums[idx], combine, idx);
+            // 如果可以选择重复的数，idx 不变
+            // dfs(target - nums[idx], idx);
+            // 如果不选择重复的数，idx 在此 +1
+            dfs(target - nums[idx], idx + 1);
             combine.remove(combine.size() - 1);
         }
     }
@@ -53,7 +56,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.combinationSum(new int[]{3, 2, 7}, 18).forEach(System.out::println);
+        solution.combinationSum(new int[]{3, 2, 7, 4,5}, 9).forEach(System.out::println);
     }
 
 }

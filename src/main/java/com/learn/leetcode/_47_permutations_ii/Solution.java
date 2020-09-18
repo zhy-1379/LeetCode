c2fb95889e27ea535dfb0e313687b19e3d2bfcaa
seaXll <2252593269@qq.com>
2020-09-18 22:59:00 +0800

    LeetCode 第 47 题：

---
+++ src/main/java/com/learn/leetcode/_47_permutations_ii/Solution.java
@@ -0,0 +1,48 @@
+package com.learn.leetcode._47_permutations_ii;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+public class Solution {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        // 排序，以便剪枝
+        Arrays.sort(nums);
+
+        boolean[] used = new boolean[len];
+        Deque<Integer> path = new ArrayDeque<>(len);
+        dfs(nums, len, 0, used, path, res);
+        return res;
+    }
+
+    private void dfs(int[] nums, int len, int depth, boolean[] used, Deque<Integer> path, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+
+        for (int i = 0; i < len; ++i) {
+            if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) {
+                continue;
+            }
+
+            path.addLast(nums[i]);
+            used[i] = true;
+
+            dfs(nums, len, depth + 1, used, path, res);
+            used[i] = false;
+            path.removeLast();
+        }
+    }
+}
+

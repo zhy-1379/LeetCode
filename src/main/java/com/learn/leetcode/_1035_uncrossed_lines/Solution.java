8265a9d524534307a7b9733cf95714ea3b166970
zhy <2252593269@qq.com>
2021-06-09 21:26:13 +0800

    add

---
+++ src/main/java/com/learn/leetcode/_1035_uncrossed_lines/Solution.java
@@ -0,0 +1,25 @@
+package com.learn.leetcode._1035_uncrossed_lines;
+
+/**
+ * @author zhy
+ */
+public class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            int num1 = nums1[i - 1];
+            for (int j = 1; j <= len2; j++) {
+                int num2 = nums2[j - 1];
+                if (num1 == num2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
+

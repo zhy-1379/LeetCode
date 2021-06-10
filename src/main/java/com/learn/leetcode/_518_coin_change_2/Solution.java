a9b475586e53dfaed9360f02b66a20b1030f3127
zhy <2252593269@qq.com>
2021-06-10 21:31:25 +0800

    add _518_coin_change_2

---
+++ src/main/java/com/learn/leetcode/_518_coin_change_2/Solution.java
@@ -0,0 +1,20 @@
+package com.learn.leetcode._518_coin_change_2;
+
+/**
+ * @author zhy
+ */
+public class Solution {
+
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
+
+
